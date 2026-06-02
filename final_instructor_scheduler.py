635c0b59bfa2882cd27445bee6204dcbd4e09c00
JoshMcKPhysics <jrmckenney@alumni.unc.edu>
2026-06-01 23:38:47 -0400

    fix install

--- final_instructor_scheduler.py
+++ final_instructor_scheduler.py
@@ -1,12 +1,4 @@
 
-import subprocess
-import sys
-    
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install('streamlit_extras')
-
 import calendar
 import json
 from pathlib import Path

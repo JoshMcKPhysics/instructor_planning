904b67bec73a6c4b4d28aefdcade699cae2dcee1
JoshMcKPhysics <jrmckenney@alumni.unc.edu>
2026-06-04 04:32:38 -0400

    try load

--- final_instructor_scheduler.py
+++ final_instructor_scheduler.py
@@ -51,14 +51,14 @@ def save_state():
     Path(STATE_FILE).write_text(json.dumps(st.session_state.selected))
 
 def load_from_db():
-    st.session_state.selected = (
+    st.session_state
+    .selected = (
         supabase
         .table("schedule_state")
         .select("*")
         .execute()
         .data
-    )
-    print(st.session_state.selected)
+    )[0]['selected']
 
 def save_to_db():
     (
